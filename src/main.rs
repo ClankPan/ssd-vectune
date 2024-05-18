66cb0b7c10c4cc27c6613bd11b53bcca34c96144
clankpan <clankpan26cm@gmail.com>
2024-05-18 15:24:27 -0400

    add write edges func

--- src/main.rs
+++ src/main.rs
@@ -32,30 +32,59 @@ fn serialize_node<'a>(
     point: &'a Point,
     edges: &'a Edges,
 ) -> (&'a SerializedPoint, &'a SerializedEdges) {
+    let serialize_point: &SerializedPoint = serialize_point(point);
+    let serialize_edges: &SerializedEdges = serialize_edges(edges);
+    (serialize_point, serialize_edges)
+}
+
+fn serialize_point<'a>(
+    point: &'a Point,
+) -> &'a SerializedPoint {
     let serialize_point: &SerializedPoint = bytemuck::cast_slice(point)
         .try_into()
         .expect("Failed to try into &[u8; DIM*4]");
+    serialize_point
+}
+
+fn serialize_edges<'a>(
+    edges: &'a Edges,
+) -> &'a SerializedEdges {
     let serialize_edges: &SerializedEdges = bytemuck::cast_slice(edges)
         .try_into()
         .expect("Failed to try into &[u8; DIGREE*4]");
-    (serialize_point, serialize_edges)
+    serialize_edges
 }
 
 fn deserialize_node<'a>(
     serialize_point: &'a SerializedPoint,
     serialize_edges: &'a SerializedEdges,
 ) -> (&'a Point, &'a Edges) {
+    let point: &Point = deserialize_point(serialize_point);
+    let edges: &Edges = deserialize_edges(serialize_edges);
+
+    (point, edges)
+}
+
+fn deserialize_point<'a>(
+    serialize_point: &'a SerializedPoint
+) -> &'a Point {
     let point: &Point = bytemuck::try_cast_slice(serialize_point)
         .expect("Failed to deserialize embeddings")
         .try_into()
         .expect("Failed to try into &[f32; DIM]");
 
+    point
+}
+
+fn deserialize_edges<'a>(
+    serialize_edges: &'a SerializedEdges,
+) -> &'a Edges {
     let edges: &Edges = bytemuck::try_cast_slice(serialize_edges)
         .expect("Failed to deserialize embeddings")
         .try_into()
         .expect("Failed to try into &[f32; DIM]");
 
-    (point, edges)
+    edges
 }
 
 // offset is address of the node in a sector buffer
@@ -67,7 +96,8 @@ fn store_index_to_sector_index_and_offset(store_index: &StoreIndex) -> (SectorIn
 
 
 trait Storage {
-    fn read(&self, offset: u64, dst: &mut [u8]);
+    fn read(&self, offset: usize, dst: &mut [u8]);
+    fn write(&mut self, offset: usize, src: &[u8]);
 }
 
 struct Cache<S: Storage> {
@@ -79,8 +109,13 @@ struct Cache<S: Storage> {
 impl<S> Cache<S>
 where
     S: Storage,
-{
-    fn read(&mut self, store_index: &StoreIndex) -> (&Point, &Edges) {
+{   
+    fn clear(&mut self) {
+        self.cache_table = vec![(usize::MAX, SystemTime::now()); self.cache_table.len()];
+        self.cache =  vec![[0; SECTOR_SIZE]; self.cache.len()];
+    }
+
+    fn read_node(&mut self, store_index: &StoreIndex) -> (&Point, &Edges) {
         let (sector_index, offset_in_sector) = store_index_to_sector_index_and_offset(store_index);
         let sector = if self.cache_table[sector_index].0 == usize::MAX {
             // Eviction
@@ -92,7 +127,7 @@ where
                 .0;
             // Write data to cache
             let buffer = &mut self.cache[cache_index];
-            self.storage.read((sector_index * SECTOR_SIZE) as u64, buffer);
+            self.storage.read(sector_index * SECTOR_SIZE, buffer);
             // Update cache table
             self.cache_table[sector_index].0 = cache_index;
             &self.cache[cache_index]
@@ -107,5 +142,15 @@ where
         let serialized_edges: &SerializedEdges = sector[offset_in_sector + POINT_SIZE..offset_in_sector + POINT_SIZE + EDGES_SIZE].try_into().expect("cannot convert to  &SerializedEdges");
         let (point, edges) = deserialize_node(serialized_point, serialized_edges);
         (point, edges)
+    }
+
+    fn write_edges(&mut self, store_index: &StoreIndex, edges: &Edges) {
+        let (sector_index, offset_in_sector) = store_index_to_sector_index_and_offset(store_index);
+        self.cache_table[sector_index].0 = usize::MAX; // This cache is no longer used
+        let serialized_edges = serialize_edges(edges);
+        let offset = sector_index * SECTOR_SIZE + offset_in_sector + POINT_SIZE; // First address of the edges
+        self.storage.write(offset, serialized_edges);
+
+        // wip: dirty_sector_table.insert(sector_index);
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -28,54 +28,88 @@ fn serializing() {
     assert_eq!(&edges, deseri_edges);
 }
 
+struct TestStorage {
+  memory: Vec<u8>
+}
+impl TestStorage {
+  pub fn new(num_sector: usize, rng: &mut SmallRng) -> Self {
+    let point: Point = gen_random_point(rng)
+        .try_into()
+        .expect("cannot convert to [f32; DIM]");
+    let edges: Edges = gen_random_edges(rng)
+        .try_into()
+        .expect("cannot convert to [Id; DIGREE");
+    let (seri_point, seri_edges): (&SerializedPoint, &SerializedEdges) = serialize_node(&point, &edges);
+    let mut memory = vec![0; SECTOR_SIZE*num_sector];
+
+    let store_index = 10;
+    let (sector_index, offset_in_sector) = store_index_to_sector_index_and_offset(&store_index);
+
+    let sector_offset = sector_index * SECTOR_SIZE;
+    let offset = sector_offset + offset_in_sector;
+    memory[offset..offset+POINT_SIZE].copy_from_slice(seri_point);
+    memory[offset+POINT_SIZE..offset+POINT_SIZE+EDGES_SIZE].copy_from_slice(seri_edges);
+
+    Self {
+      memory,
+    }
+  }
+}
+impl Storage for TestStorage {
+  fn read(&self, offset: usize, dst: &mut [u8]) {
+    let end = offset + dst.len();
+    dst.copy_from_slice(&self.memory[offset..end]);
+  }
+
+  fn write(&mut self, offset: usize, src: &[u8]) {
+    let end = offset + src.len();
+    self.memory[offset..end].copy_from_slice(src);
+  }
+}
+
+
 #[test]
 fn reading_from_cache() {
-  struct TestStorage {
-    memory: Vec<u8>
-  }
-  impl TestStorage {
-    pub fn new(num_sector: usize) -> Self {
-
-      let mut rng = SmallRng::seed_from_u64(123456789);
-      let point: Point = gen_random_point(&mut rng)
-          .try_into()
-          .expect("cannot convert to [f32; DIM]");
-      let edges: Edges = gen_random_edges(&mut rng)
-          .try_into()
-          .expect("cannot convert to [Id; DIGREE");
-      let (seri_point, seri_edges): (&SerializedPoint, &SerializedEdges) = serialize_node(&point, &edges);
-      let mut memory = vec![0; SECTOR_SIZE*num_sector];
-
-      let store_index = 10;
-      let (sector_index, offset_in_sector) = store_index_to_sector_index_and_offset(&store_index);
-
-      let sector_offset = sector_index * SECTOR_SIZE;
-      let offset = sector_offset + offset_in_sector;
-      memory[offset..offset+POINT_SIZE].copy_from_slice(seri_point);
-      memory[offset+POINT_SIZE..offset+POINT_SIZE+EDGES_SIZE].copy_from_slice(seri_edges);
-
-      Self {
-        memory,
-      }
-    }
-  }
-  impl Storage for TestStorage {
-    fn read(&self, offset: u64, dst: &mut [u8]) {
-      let offset = offset as usize;
-      let end = offset + dst.len();
-      dst.copy_from_slice(&self.memory[offset..end]);
-    }
-  }
-
   let num_sector = 100;
+  let mut rng = SmallRng::seed_from_u64(123456789);
   let mut cache = Cache {
     cache_table: vec![(usize::MAX, SystemTime::now()); num_sector],
     cache: vec![[0; SECTOR_SIZE]; num_sector],
-    storage: TestStorage::new(num_sector),
+    storage: TestStorage::new(num_sector, &mut rng),
   };
 
-  let a = cache.read(&10);
+  let a = cache.read_node(&10);
 
   println!("{:?}", a);
+
+}
+
+#[test]
+fn rewrite_edges() {
+  let num_sector = 100;
+  let mut rng = SmallRng::seed_from_u64(123456789);
+  let mut cache = Cache {
+    cache_table: vec![(usize::MAX, SystemTime::now()); num_sector],
+    cache: vec![[0; SECTOR_SIZE]; num_sector],
+    storage: TestStorage::new(num_sector, &mut rng),
+  };
+
+  let (_, original_edges) = cache.read_node(&10);
+  let original_edges: Edges = original_edges.to_vec()[..].try_into().unwrap();
+  println!("{:?}", original_edges);
+
+
+  cache.write_edges(&10, &original_edges);
+
+  let (_, rewrited_edges) = cache.read_node(&10);
+
+  assert_eq!(rewrited_edges, &original_edges);
+
+  let random_edges: Edges = gen_random_edges(&mut rng)[..].try_into().unwrap();
+  cache.write_edges(&10, &random_edges);
+  let (_, random_rewrited_edges) = cache.read_node(&10);
+
+  println!("{:?}", random_rewrited_edges);
+  assert_eq!(random_rewrited_edges, &random_edges);
 
 }
